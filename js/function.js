be4e5521d6fdb2ed708bf26059ae9c6b8fcd95c2
Vinicius Ferreira <viniciuferreira32@gmail.com>
2021-10-28 23:29:15 -0300

    Mais alguns ajuste na janela modal de lista

--- js/function.js
+++ js/function.js
@@ -56,33 +56,62 @@ $(function(){
 
     function addCart(){
 
-        $('.car').click(function(){
+        /*$('.car').click(function(){
             $('.car > svg').css('stroke','#f3791c');
-        })
+        })*/
 
 
         $('.cart').click(function(){
             let backgroundCurrent = $('.image-single:first-of-type').css('background-image');
+ 
             /*Para pode tornar automatico quando faz ser a single page*/
-            $('.img-cart').css('background-image',backgroundCurrent).css('background-position','center').css('background-size','contain')
+            /*Adicionar dinamicamente informações da pagina no lista de compras*/
+            $('.img-cart').css('background-image',backgroundCurrent).css('background-position','center').css('background-size','100%')
             $('.valueSingle').html($('.price-discount > p:first-of-type').text()
             );
-
+            $('.info-cart > p').html($('.info > h1').text())
+            $('.p-wraper  p:nth-of-type(2)').html('x');
+            $('.cart-amount').html($('.amount p:nth-of-type(2)').text())
+            $('.cart-amount').fadeIn();
+            $('.p-wraper > p:nth-of-type(3)').html($('.amount p:nth-of-type(2)').text());
             /**/
-            $('.cart-wraper > span:nth-of-type(3)').html($('.amount p:nth-of-type(2)').text());
 
+            /*Calculo automatico do valor vezes a quantidade*/
            var total = eval($('.amount p:nth-of-type(2)').text() + '*' + $('.valueSingle').text().substr(1,20));
 
            var totalUSD = total.toLocaleString('USD',{style: 'currency', currency: 'USD'});
 
-           $('.cart-wraper span:nth-of-type(2)').html('x');
-           $('.cart-wraper > span:last-of-type').html(totalUSD.substr(2,10));
+           $('.p-wraper  p:last-of-type').html(totalUSD.substr(2,10));
+           /**/ 
 
-           $('.cart-amount').html($('.amount p:nth-of-type(2)').text())
-           $('.cart-amount').fadeIn();
+           /*Fazer a janela de lista de compras aparecer e deseparecer*/ 
+           $('.car > svg').click(function(){
+            $('.list-wraper').fadeIn();
+
+                if($('.list-wraper').fadeIn() !== false){
+                    $('.car svg').click(function(){
+                        $('.list-wraper').fadeOut();
+                    })
+                }else{
+                }
+           })
+           /**/
 
         })
     }
 
 
 })
+
+/*OBS:
+        *Arrumar o problema de fadeIn e fadeOut na list-wraper.
+
+        *Ajustar o input button e o icon da lixeira.
+
+        *Colocar que quando a list estiver vazia aparecer "Your cart is empty".
+
+        *Quando adiciona mais de um lista os valores onde tem o titulo, x e o 
+        resultado somem.
+        *E ajuste na estilização da lista modal.
+        
+*/ 
